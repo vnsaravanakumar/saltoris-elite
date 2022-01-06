776786d06ca1e182b55464cc37e553bc30736405
vnsaravanakumar <vnsaravanakumar@gmail.com>
2022-01-06 08:24:44 -0800

    upgraded to tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.js', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
+    content: ['./src/**/*.js', './public/index.html'],
     theme: {
         fontFamily: {
             sans: ['Roboto', 'sans-serif'],
             serif: ['"Roboto Slab"', 'serif'],
             body: ['Roboto', 'sans-serif'],
         },
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
