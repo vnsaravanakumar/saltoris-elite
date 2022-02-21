ba967fb9cd4f4e50ee72fd661b76549dfaaa9e3a
vnsaravanakumar <vnsaravanakumar@gmail.com>
2022-02-21 10:26:42 -0800

    added the final change for status card

--- src/components/dashboard/CustomizableStatusCard.js
+++ src/components/dashboard/CustomizableStatusCard.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DashboardStatusCard from './DashboardStatusCard';
 import { Responsive, WidthProvider } from "react-grid-layout";
 const ResponsiveReactCardLayout = WidthProvider(Responsive);
@@ -6,20 +6,21 @@ const ResponsiveReactCardLayout = WidthProvider(Responsive);
 
 const setColumnPartition = (cols, setCols, totalItems) => {
   let noCols = cols["lg"];
-  debugger;
+
   if(Math.ceil(noCols/totalItems) !== noCols/totalItems){
     Object.keys(cols).map(itemKey => {
       cols[itemKey] = Math.ceil(noCols/totalItems) * totalItems
     })
-    setCols(cols);
+    setCols({...cols});
   }
 }
 
-const generateLayout = (cols, setCols) => {
-  const totalItems = Object.keys(statusCardItems).length;
+const generateLayout = (cols, setCols, toolbox) => {
+  const selectedItems = getSelectedItems(toolbox);
+  const totalItems = selectedItems.length;
   const result = {};
 
-  setColumnPartition(cols, setCols, totalItems);
+  //setColumnPartition(cols, setCols, totalItems);
 
   Object.keys(cols).map((colKey, key) => {
     key++;
@@ -27,7 +28,7 @@ const generateLayout = (cols, setCols) => {
     const direction = (colKey === "xs" || colKey === "xxs") ? "row" : "col";
     const width = direction === "col" ? (col / totalItems) : col;
 
-    result[colKey] = Object.keys(statusCardItems).map((item, itemKey) => {
+    result[colKey] = selectedItems.map((item, itemKey) => {
       itemKey++;
       return {
         w: width,
@@ -39,9 +40,43 @@ const generateLayout = (cols, setCols) => {
     })
   })
 
+  console.log(cols);
+  console.log(result);
   return result;
 }
 
+const fourLayout = {
+  lg: [
+    {w: 3, h: 4, x: 0, y: 0, i: '1'},
+    {w: 3, h: 4, x: 3, y: 0, i: '2'},
+    {w: 3, h: 4, x: 6, y: 0, i: '3'},
+    {w: 3, h: 4, x: 9, y: 0, i: '4'}
+  ],
+  md: [
+    {w: 3, h: 4, x: 0, y: 0, i: '1'},
+    {w: 3, h: 4, x: 3, y: 0, i: '2'},
+    {w: 3, h: 4, x: 6, y: 0, i: '3'},
+    {w: 3, h: 4, x: 9, y: 0, i: '4'}
+  ],
+  sm: [
+    {w: 3, h: 4, x: 0, y: 0, i: '1'},
+    {w: 3, h: 4, x: 3, y: 0, i: '2'},
+    {w: 3, h: 4, x: 6, y: 0, i: '3'},
+    {w: 3, h: 4, x: 9, y: 0, i: '4'}
+  ],
+  xs: [
+    {w: 3, h: 4, x: 0, y: 0, i: '1'},
+    {w: 3, h: 4, x: 3, y: 0, i: '2'},
+    {w: 3, h: 4, x: 6, y: 0, i: '3'},
+    {w: 3, h: 4, x: 9, y: 0, i: '4'}
+  ],
+  xxs: [
+    {w: 3, h: 4, x: 0, y: 0, i: '1'},
+    {w: 3, h: 4, x: 3, y: 0, i: '2'},
+    {w: 3, h: 4, x: 6, y: 0, i: '3'},
+    {w: 3, h: 4, x: 9, y: 0, i: '4'}
+  ]
+}
 const statusCardLayouts = {
     lg: [
       {w: 2, h: 4, x: 0, y: 0 , i: '1'},
@@ -80,38 +115,42 @@ const statusCardLayouts = {
     ]
   };
 
-const statusCardItems = {
-  "1": {
+const statusCardItems = [
+  {
+    id: "1",
     color: "pink",
-    icon: "trending_up",
-    title: "Pending Trade Requests",
+    icon: "ad_units",
+    title: "Orders",
     amount: "25",
     percentage: "3.48",
     percentageIcon: "arrow_upward",
     percentageColor: "green",
     date: "Since last month",
   },
-  "2": {
+  {
+    id: "2",
     color:"pink",
     icon: "trending_up",
-    title: "Pending Trade Requests",
+    title: "Opportunities",
     amount: "25",
     percentage: "3.48",
     percentageIcon: "arrow_upward",
     percentageColor: "green",
     date: "Since last month"
   },
-  "3": {
+  {
+    id: "3",
     color: "orange",
     icon: "groups",
-    title: "Open Orders",
+    title: "Pending Notifications",
     amount: "56",
     percentage: "3.48",
     percentageIcon: "arrow_downward",
     percentageColor: "red",
     date: "Since last week"
   },
-  "4": {
+  {
+    id: "4",
     color: "purple",
     icon: "paid",
     title: "Outstanding Invoices",
@@ -121,7 +160,8 @@ const statusCardItems = {
     percentageColor: "orange",
     date: "Since yesterday"
   },
-  "5": {
+  {
+    id: "5",
     color: "blue",
     icon: "poll",
     title: "Active Bids",
@@ -131,22 +171,58 @@ const statusCardItems = {
     percentageColor: "green",
     date: "Since last month"
   }
+]
+/*
+,
+  {
+    id: "5",
+    color: "blue",
+    icon: "poll",
+    title: "Active Bids",
+    amount: "9",
+    percentage: "12",
+    percentageIcon: "arrow_upward",
+    percentageColor: "green",
+    date: "Since last month"
+  }
+*/
+const getSelectedItems = (toolbox) => {
+  const toolboxIds = toolbox.map(item => item.id)
+  return statusCardItems.filter(item => !toolboxIds.includes(item.id))
 }
+
 export default function CustomizableStatusCard() {
-    const [toolbox, setToolbox] = useState({ lg: [] });
+    const [toolbox, setToolbox] = useState([]);
     const currentBreakpoint = "lg";
     const [isEdit, setIsEdit] = useState(true);
     const [cols, setCols] = useState({ lg: 10, md: 10, sm: 10, xs: 10, xxs: 10 });
+    const [layout, setLayout] = useState();
+
+    const onPutItem = (item) => {
+      setToolbox([...toolbox, item])
+    }
+
+    const onGetItem = (selecteItem) => {
+      setToolbox([...toolbox.filter(item => item.id !== selecteItem.id)])
+    }
+
+    // useEffect(()=>{
+    //   setLayout({...generateLayout(cols, setCols, toolbox)})
+    // }, [])
+
+    // useEffect(()=>{
+    //   setLayout({...generateLayout(cols, setCols, toolbox)})
+    // }, [toolbox])
 
     return (
       <>
         <ToolBox
-          items={toolbox[currentBreakpoint] || []}
-          onTakeItem={onTakeItem}
+          items={toolbox || []}
+          onTakeItem={onGetItem}
         />
         <ResponsiveReactCardLayout
           className="layout"
-          layouts={generateLayout(cols, setCols)}
+          layouts={generateLayout(cols, setCols, toolbox)}
          // onBreakpointChange={this.handleBreakPointChange}
           isDraggable
           isRearrangeable
@@ -157,13 +233,13 @@ export default function CustomizableStatusCard() {
           cols={cols}
         >
           {
-            Object.keys(statusCardItems).map(key => {
-              return  <div key={key}>
-                {isEdit && <div className="cursor-pointer mr-4 absolute right-0" onClick={()=>{onPutItem(key)}}>
+            getSelectedItems(toolbox).map((item, key) => {
+              return  <div key={key+1}>
+                {isEdit && <div className="cursor-pointer mr-4 absolute right-0" onClick={()=>{onPutItem(item)}}>
                   &times;
                 </div>}
                 <DashboardStatusCard
-                    {...statusCardItems[key]}
+                    {...item}
                     edit={isEdit}
                 />
               </div>
@@ -174,27 +250,22 @@ export default function CustomizableStatusCard() {
       )
 }
 
-const onPutItem = () => {
-  console.log(generateLayout());
-}
-const onTakeItem = () => {
-
-}
-const ToolBoxItem = ({item}) => {
+const ToolBoxItem = ({item, onTakeItem}) => {
     return (
       <div
-        className="toolbox__items__item"
+        className="bg-white w-fit p-2 relative cursor-pointer rounded-md"
         onClick={onTakeItem.bind(undefined, item)}
       >
-        {item.i}
+        <span className='absolute right-0 top-0 p-1'>+</span>
+        <span className='pt-5 pr-3'>{item.title}</span>
       </div>
     );
 }
-const ToolBox = ({items}) => {
+const ToolBox = ({items, onTakeItem}) => {
     return (
-      <div className="toolbox">
-        <span className="toolbox__title">Toolbox</span>
-        <div className="toolbox__items">
+      <div className="toolbox pl-3 pt-2 bg-gray-200">
+        {/* <span className="toolbox__title">Toolbox</span> */}
+        <div className="flex gap-2">
           {items.map(item => (
             <ToolBoxItem
               key={item.i}
