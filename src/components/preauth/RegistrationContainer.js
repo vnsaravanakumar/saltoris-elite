395e0b5da0e683827a922872aebd7f4fc199a262
vnsaravanakumar <vnsaravanakumar@gmail.com>
2022-04-11 07:17:37 -0700

    updated the position for registration

--- src/components/preauth/RegistrationContainer.js
+++ src/components/preauth/RegistrationContainer.js
@@ -1,6 +1,6 @@
 export default function Container({ children, className }) {
     return (
-        <div className={"absolute  m-auto left-0 right-0  " + className}>
+        <div className={"absolute w-auto m-auto left-0 right-0 translate-y-52 " + className}>
             <div >{children}</div>
         </div>
     );
